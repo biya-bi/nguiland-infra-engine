d7680d06a8403f4f3159f8586722344954fd3adb
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-07-23 21:09:22 -0400

    Improve the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,28 @@ get_sops_age_private_key() {
   echo $(grep -e "${regex}" "${sops_age_key_file}" -A 1 | grep -v -e "${regex}")
 }
 
+create_namespace() {
+  local namespace="$1"
+
+  local result=$(kubectl get namespaces | tail -n +2 | awk '{print $1}' | grep "${namespace}")
+
+  if [ "${result}" != "${namespace}" ]; then
+      kubectl create namespace "${namespace}"
+  fi
+}
+
+create_age_key_secret() {
+  local namespace="$1"
+  local secret_name="$2"
+  local sops_age_private_key="$3"
+
+  local result=$(kubectl get secrets --namespace="${namespace}" | tail -n +2 | awk '{print $1}' | grep "${secret_name}")
+
+  if [ "${result}" != "${secret_name}" ]; then
+    kubectl create secret generic "${secret_name}" --namespace="${namespace}" --from-literal=identity.agekey="${sops_age_private_key}"
+  fi
+}
+
 create_sops_age_secret() {
   local namespace="$1"
   local sops_age_key_file="$2"
@@ -25,9 +47,11 @@ create_sops_age_secret() {
     printf "The '%s' environment variable points to the '%s' file. \nDo you what to use the later file for the deployment?\n" "SOPS_AGE_KEY_FILE" "${sops_age_key_file}"
     local response=$(yes_or_no)
     if [ "${response}" == "Yes" ]; then
+      create_namespace "${namespace}"
+
       local sops_age_private_key=$(get_sops_age_private_key "${sops_age_key_file}")
-      kubectl create namespace "${namespace}" --dry-run=client -o yaml | grep -v "\s*creationTimestamp:\s*null" | kubectl apply -f -
-      kubectl create secret generic sops-age --namespace="${namespace}" --from-literal=identity.agekey="${sops_age_private_key}" --dry-run=client -o yaml | grep -v "\s*creationTimestamp:\s*null" | kubectl apply -f -
+
+      create_age_key_secret "${namespace}" "sops-age" "${sops_age_private_key}"
     fi
   fi
 }
