1f6807298c97b9a42566a796b3e6aa22ad6a203b
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2026-04-19 12:44:13 -0400

    feat(scripts): add stop.sh to stop the engine

---
+++ scripts/stop.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+kubectl delete validatingwebhookconfigurations kyverno-resource-validating-webhook-cfg validate.kyverno.svc-fail --ignore-not-found
+kubectl delete mutatingwebhookconfigurations kyverno-resource-mutating-webhook-cfg kyverno-policy-mutating-webhook-cfg --ignore-not-found
+kubectl delete validatingwebhookconfigurations ingress-nginx-admission --ignore-not-found
+
+flux uninstall --namespace infra --silent
+flux uninstall --namespace kyverno --silent
+flux uninstall --namespace flux-system --silent
