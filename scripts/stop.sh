e0502baf242af5522e7b66d66f1975483702d3a2
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2026-04-19 20:26:42 -0400

    chore(scripts): improve full teardown sequence

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+flux uninstall --namespace flux-system --silent
+flux uninstall --namespace infra --silent
+flux uninstall --namespace kyverno --silent
+
 kubectl get validatingwebhookconfigurations -o name | xargs -I {} kubectl delete {}
 kubectl get mutatingwebhookconfigurations -o name | xargs -I {} kubectl delete {}
 kubectl get crds -o name | xargs -I {} kubectl delete {}
-
-flux uninstall --namespace infra --silent
-flux uninstall --namespace kyverno --silent
-flux uninstall --namespace flux-system --silent
