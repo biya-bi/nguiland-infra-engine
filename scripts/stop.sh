e016f5c439a3a1e975ce983f747b4362b334612b
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2026-04-19 20:57:27 -0400

    chore(scripts): delete Tekton namespaces during teardown

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,8 +3,11 @@
 flux uninstall --namespace flux-system --silent
 flux uninstall --namespace infra --silent
 flux uninstall --namespace kyverno --silent
+flux uninstall --namespace tekton-pipelines --silent
 
 kubectl delete validatingwebhookconfigurations --all
 kubectl delete mutatingwebhookconfigurations --all
 
 kubectl get crds -o name | xargs -I {} kubectl delete {}
+
+kubectl delete namespace tekton-dashboard tekton-pipelines tekton-pipelines-resolvers --ignore-not-found
